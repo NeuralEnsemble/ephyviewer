4824e830f38793239062f15b10eea6f84def235d
Samuel Garcia <sam/garcia.die@gmail.com>
2017-08-24 18:05:45 +0200

    Readme

--- README.md
+++ README.md
@@ -3,3 +3,36 @@
 Simple viewers for ephys signals and related stuff (signal, spikes, events, triggers, video, ...)
 Based on PyQt5 and pyqtgraph.
 
+
+Can be use at two level:
+
+# standalone application to open file supported by neo.rawio
+
+launch from console:
+```
+ephyviewer
+```
+
+
+
+# To script in python some customs viewers for dataset with theses kind of codes:
+
+```pyhton
+import ephyviewer
+import numpy as np
+
+app = ephyviewer.mkQApp()
+
+#signals
+sigs = np.random.rand(100000,16)
+sample_rate = 1000.
+t_start = 0.
+view1 = ephyviewer.TraceViewer.from_numpy(sigs, sample_rate, t_start, 'Signals')
+
+win = ephyviewer.MainViewer(debug=True, show_auto_scale=True)
+win.add_view(view1)
+win.show()
+
+app.exec_()
+
+```
--- ephyviewer/standalone.py
+++ ephyviewer/standalone.py
@@ -87,33 +87,7 @@ class StandAloneViewer(MainViewer):
             view = EpochViewer(source=ep_source, name='epochs')
             self.add_view(view)
         
-        
 
-    #~ {'': <class 'neo.rawio.tdtrawio.TdtRawIO'>, '': ['/tmp/files_for_testing_neo/tdt/aep_05'], '': {}}
-    
-    #~ dirname = '/tmp/files_for_testing_neo/tdt/aep_05/'
-    #~ neorawio = TdtRawIO(dirname=dirname)
-    #~ neorawio.parse_header()
-    #~ print(neorawio)
-    
-    #~ sources = ephyviewer.get_source_from_neo(neorawio)
-    
-
-    #~ app = ephyviewer.mkQApp()
-    #~ win = ephyviewer.MainViewer(debug=True, show_auto_scale=True)
-    
-    #~ for i, sig_source in enumerate(sources['signal']):
-        #~ view = ephyviewer.TraceViewer(source=sig_source, name='signal {}'.format(i))
-        #~ win.add_view(view)
-
-    #~ for i, ep_source in enumerate(sources['epoch']):
-        #~ view = ephyviewer.EpochViewer(source=ep_source, name='epochs')
-        #~ win.add_view(view)
-
-            
-            
-        
-        
 
 
 class RawNeoOpenDialog(QT.QDialog):
