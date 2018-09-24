3c9dd7669d18be8164ca9a58ab1a01d51c496f89
Samuel Garcia <sam.garcia.die@gmail.com>
2018-09-24 11:52:56 +0200

    Add class method to traceviewer

--- ephyviewer/tests/test_traceviewer.py
+++ ephyviewer/tests/test_traceviewer.py
@@ -2,6 +2,9 @@ import numpy as np
 import ephyviewer
 from  ephyviewer.tests.testing_tools import make_fake_signals, make_fake_signals_with_scatter
 
+import neo
+import quantities as pq
+
 
 def test_traceviewer():
     source = make_fake_signals()
@@ -34,8 +37,44 @@ def test_traceviewer_with_scatter():
     win.show()
     
     app.exec_()    
+
+
+
+def test_traceviewer_cls_method_numpy():
+    sigs = np.random.rand(100000,16)
+    sample_rate = 1000.
+    t_start = 0.
+    
+    app = ephyviewer.mkQApp()
+    view = ephyviewer.TraceViewer.from_numpy(sigs, sample_rate, t_start, 'sigs')
+    
+    win = ephyviewer.MainViewer(debug=True, show_auto_scale=True)
+    win.add_view(view)
+    win.show()
+    app.exec_()    
+
+def test_traceviewer_cls_method_neo():
+    sigs = np.random.rand(100000,16)
+    sample_rate = 1000.
+    t_start = 0.
+    
+    neo_anasig = neo.AnalogSignal(sigs*pq.mV, sampling_rate=sample_rate*pq.Hz, t_start=0*pq.s, copy=True)
+    print(neo_anasig)
     
 
+    app = ephyviewer.mkQApp()
+    
+    view = ephyviewer.TraceViewer.from_neo_analogsignal(neo_anasig, 'sigs')
+    win = ephyviewer.MainViewer(debug=True, show_auto_scale=True)
+    win.add_view(view)
+    win.show()
+    app.exec_()    
+
+
+
 if __name__=='__main__':
-    test_traceviewer()
+    #~ test_traceviewer()
     #~ test_traceviewer_with_scatter()
+    #~ test_traceviewer_cls_method_numpy()
+    test_traceviewer_cls_method_neo()
+    
