a9cf590d663ec41d5839b2b13dc9f171031cda5d
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2019-07-13 17:18:21 -0400

    Add docs sidebar description and GitHub button

--- doc/conf.py
+++ doc/conf.py
@@ -91,7 +91,16 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'description': 'Simple viewers for ephys signals, events, video and more',
+    'fixed_sidebar': True,
+    'github_user': 'NeuralEnsemble',
+    'github_repo': 'ephyviewer',
+    'github_button': True,
+    'github_type': 'watch', # watch, fork, follow, star
+    'github_count': False,
+    # 'github_banner': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
