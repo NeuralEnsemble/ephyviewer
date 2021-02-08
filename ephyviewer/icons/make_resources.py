57a1ca3861cae1d3e0fa6e1e010969d9083fac5e
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2021-02-08 13:04:49 -0500

    Add script arg for Qt bindings to make_resources.py

--- ephyviewer/icons/make_resources.py
+++ ephyviewer/icons/make_resources.py
@@ -1,11 +1,8 @@
 import os
 import sys
 
-from ephyviewer.myqt import QT_MODE
-print('QT_MODE', QT_MODE)
 
-
-def make_resources_icons():
+def make_resources_icons(qt_mode):
     with open('icons.qrc','w') as f:
         f.write("""<!DOCTYPE RCC><RCC version="1.0">
     <qresource>
@@ -19,19 +16,29 @@ def make_resources_icons():
 </RCC>
 """)
 
-    if QT_MODE=='PyQt4':
+    if qt_mode == 'PyQt4':
         if sys.version_info > (3,):
             os.popen('pyrcc4 -py3 icons.qrc -o icons_py3_PyQt4.py')
         else:
             os.popen('pyrcc4 icons.qrc -o icons_py2_PyQt4.py')
 
-    elif QT_MODE=='PyQt5':
+    elif qt_mode == 'PyQt5':
         if sys.version_info > (3,):
             os.popen('pyrcc5 icons.qrc -o icons_py3_PyQt5.py')
         else:
             raise(NotImplementedError)
 
+    else:
+        raise ValueError('Cannot build icons for unrecognized Qt bindings: ' + qt_mode)
+
 
 #------------------------------------------------------------------------------
 if __name__ == '__main__' :
-    make_resources_icons()
+
+    if len(sys.argv) > 1:
+        qt_mode = sys.argv[1]
+    else:
+        from ephyviewer.myqt import QT_MODE as qt_mode
+
+    print('Building icons for Qt bindings:', qt_mode)
+    make_resources_icons(qt_mode)
