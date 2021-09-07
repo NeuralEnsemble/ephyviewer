f749dc4c8ed4be0586ff4145be4f0d751beabafa
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2021-09-07 13:30:34 -0400

    Uncomment some locally executable tests

--- ephyviewer/tests/test_datasource.py
+++ ephyviewer/tests/test_datasource.py
@@ -174,11 +174,11 @@ def test_spikeinterface_sources():
 
 
 if __name__=='__main__':
-    #~ test_InMemoryAnalogSignalSource()
-    #~ test_VideoMultiFileSource()
-    #~ test_InMemoryEventSource()
-    #~ test_InMemoryEpochSource()
-    #~ test_spikesource()
-    #~ test_neo_rawio_sources()
-    #~ test_neo_object_sources()
+    test_InMemoryAnalogSignalSource()
+    test_VideoMultiFileSource()
+    test_InMemoryEventSource()
+    test_InMemoryEpochSource()
+    test_spikesource()
+    test_neo_rawio_sources()
+    test_neo_object_sources()
     test_spikeinterface_sources()
